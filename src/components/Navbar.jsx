3b98ac441edfab56c9da5f50000b42417d25a5ec
AkashItlogiko <akashsahanwu089@gmail.com>
2025-02-03 19:29:42 +0600

    Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,21 +9,32 @@ const Navbar = () => {
         <div className="text-lg font-bold">
           <Link to="/">e-Shop</Link>
         </div>
-        <div>
+        <div className="relative flex-1 mx-4">
           <form>
-            <input type="text" placeholder="Serch Product" autoComplete="off" />
-            <FaSearch></FaSearch>
+            <input
+              type="text"
+              placeholder="Serch Product"
+              className="w-full border rounded-full py-2 px-2 shadow-md"
+              required
+            />
+            <FaSearch className="absolute top-3 right-3 text-red-500"></FaSearch>
           </form>
         </div>
-        <div>
+        <div className="flex items-center space-x-4">
           <Link to="/cart">
-            <FaShoppingCart />
+            <FaShoppingCart className="text-lg" />
           </Link>
-          <button>Login | Register</button>
+          <button className="hidden md:block">Login | Register</button>
           <button>
             <FaUser />
           </button>
         </div>
+      </div>
+      <div className="flex items-center justify-center space-x-10 py-4 text-sm font-bold ">
+        <Link to={'/'} className="hover:underline">Home</Link>
+        <Link to={'/'} className="hover:underline">Shop</Link>
+        <Link to={'/'} className="hover:underline">Contact</Link>
+        <Link to={'/'} className="hover:underline">About</Link>
       </div>
     </nav>
   );
