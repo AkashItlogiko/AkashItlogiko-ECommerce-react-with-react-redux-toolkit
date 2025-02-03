57fd5197707a508b3d2727b848ba4fc7847a26a9
AkashItlogiko <akashsahanwu089@gmail.com>
2025-02-03 23:38:18 +0600

    Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,14 +6,14 @@ const Home = () => {
   return (
     <div className="bg-white mt-2 px-4 md:px-16 lg:px-24">
       <div className="container mx-auto py-4 flex flex-col md:flex-row space-x-2">
-        <div className="w-full md-w-3/12">
+        <div className="w-full md:w-3/12">
           <div className="bg-red-600 text-white text-xs font-bold px-2 py-2.5">
             SHOP BY CATEGORIES
           </div>
           <ul className="space-y-4 bg-gray-100 p-3 border">
             {Categories.map((category, index) => (
               <li key={index} className="flex items-center text-sm font-medium">
-                <div className="w-2 h-2 border border-red-500 rounded-full"></div>
+                <div className="w-2 h-2 border border-red-500 rounded-full mr-2"></div>
                 {category}
               </li>
             ))}
@@ -21,7 +21,7 @@ const Home = () => {
         </div>
 
         <div className="w-full md:w-9/12 mt-8 md:mt-0 h-96 relative">
-          <img src={HeroImage} alt="h-full w-full" />
+          <img src={HeroImage} alt="" className='h-full w-full' />
           <div className="absolute top-16 left-8">
             <p className="text-gray-600 md-4">Code With Hena</p>
             <h2 className="text-3xl font-bold">WELCOME TO E-SHOP</h2>
