060f2b58a4084fd618d3b84d2e1e097135205755
AkashItlogiko <akashsahanwu089@gmail.com>
2025-02-10 00:32:25 +0600

    Done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import EmtyCart from '../assets/images/emptycart.png'
 
 const Cart = () => {
-  return <div>cart</div>;
+  const cart =useSelector(state=>state.cart)
+  return
+(<div>
+{cart.products.length>0?<div></div>:
+
+<div>
+  <img src="" alt="" />
+</div>
+} 
+</div>)
+  
 };
 
 export default Cart;
