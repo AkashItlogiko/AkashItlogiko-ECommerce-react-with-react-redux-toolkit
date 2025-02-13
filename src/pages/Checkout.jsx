b050e1ba47721f341416adb66ad262f2dd8678c1
AkashItlogiko <akashsahanwu089@gmail.com>
2025-02-13 19:00:46 +0600

    Done

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 import { FaAngleDown, FaAngleUp } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 
 const Checkout = () => {
   const [billingToggle, setBillingToggle] = useState(true);
   const [shippingToggle, setShippingToggle] = useState(false);
   const [paymentToggle, setPaymentToggle] = useState(false);
   const [paymentMethod, setPaymentMethod] = useState('cod');
+
+  const [shippingInfo, setShippingInfo] = useState({
+    address: '',
+    city: '',
+    zip: '',
+  });
+
+  const cart = useSelector(state => state.cart);
   return (
     <div className="container justify-between mx-auto py-8 min-h-96 px-4 md:px-16 lg:px-24 ">
       <h3 className="text-2xl font-semibold mb-4">CHECKOUT</h3>
@@ -135,20 +144,56 @@ const Checkout = () => {
                 <label className="block text-gray-700 ml-2">Debit Card</label>
               </div>
               {paymentMethod === 'dc' && (
-                <div>
-                  <h3>Debit Card Information</h3>
-                  <div>
-                    <label htmlFor="">Card Number</label>
-                    <input type="text" />
+                <div className="bg-gray-100 p-4 rounded-lg mb-4">
+                  <h3 className="text-xl font-semibold mb-4">
+                    Debit Card Information
+                  </h3>
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 font-semibold mb-2"
+                      htmlFor=""
+                    >
+                      Card Number
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="Enter Card Number"
+                      className="border p-2 w-full rounded"
+                      required
+                    />
                   </div>
-                  <div>
-                    <label htmlFor="">Card Holder Name</label>
-                    <input type="text" />
+                  <div className="mb-4">
+                    <label className="block text-gray-700 font-semibold mb-2">
+                      Card Holder Name
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="Enter card holder name"
+                      className="border p-2 w-full rounded"
+                      required
+                    />
                   </div>
-                  <div>
-                    <div>
-                      <label htmlFor="">Expire Date</label>
-                      <input type="text" />
+                  <div className="flex justify-between mb-4">
+                    <div className="w-1/2">
+                      <label className="block text-gray-700 font-semibold mb-2">
+                        Expire Date
+                      </label>
+                      <input
+                        type="text"
+                        placeholder="MM/YY"
+                        className="border p-2 w-full rounded"
+                        required
+                      />
+                    </div>
+                    <div className="w-1/2 ml-2">
+                      <label className="block text-gray-700 font-semibold mb-2">
+                        CVV
+                      </label>
+                      <input
+                        type="text"
+                        placeholder="CVV"
+                        className="border p-2 w-full rounded"
+                      />
                     </div>
                   </div>
                 </div>
@@ -157,7 +202,39 @@ const Checkout = () => {
           </div>
         </div>
 
-        <div className="md:w-1/3 bg-white p-6 rounded-lg shadow-md border"></div>
+        <div className="md:w-1/3 bg-white p-6 rounded-lg shadow-md border">
+          <h3 className="text-lg font-semibold mb-4">Order Summary</h3>
+          <div className="space-y-4">
+            {cart.products.map(product => (
+              <div key={product.id} className="flex justify-between">
+                <div className="flex items-center">
+                  <img
+                    src={product.Image}
+                    alt="product.name"
+                    className="w-16 h-16 object-contain rounded"
+                  />
+                  <div className="ml-4">
+                    <h4 className="text-md font-semibold">{product.name}</h4>
+                    <p className="text-gray-600">
+                      ${product.totalPrice} x {product.quantity}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="mt-4 border-t pt-4">
+            <div className="flex justify-between">
+              <span>Total Price:</span>
+              <span className="font-semibold">
+                ${cart.totalPrice.toFixed(2)}
+              </span>
+            </div>
+          </div>
+          <button className="w-full bg-red-600 text-white py-2 mt-6 hover:bg-red-800">
+            Place Order
+          </button>
+        </div>
       </div>
     </div>
   );
