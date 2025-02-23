29750aff3b0031a7ebcbab8dc43b21ebfaef3a16
AkashItlogiko <akashsahanwu089@gmail.com>
2025-02-23 12:03:53 +0600

    Done

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Import the useNavigate hook
 import ErrorPageImage from '../assets/images/ErrorPage.jpg';
 
 const Error = () => {
+  const navigate = useNavigate(); // Initialize the navigate function
+
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <img
         src={ErrorPageImage}
         alt="Error Page"
-        className="max-w-full md:max-w-lg rounded-lg  "
+        className="max-w-full md:max-w-lg rounded-lg mb-4"
       />
+      <button
+        onClick={() => navigate('/')} // Navigate to the home route
+        className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+      >
+        Go to Home
+      </button>
     </div>
   );
 };
